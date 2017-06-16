f01dcee6b021084389e667d779ef337e414ddd25
Mike Wall <mewall@lanl.gov>
2017-06-16 09:24:08 -0600

    modify test script for cctbx to use simplified API

--- scripts/test_cctbx_lunus.py
+++ scripts/test_cctbx_lunus.py
@@ -19,19 +19,12 @@ print data
 #print img
 
 A = LunusDIFFIMAGE()
-A.set_raw_data(data)
-print A.get_image_data_type_size()
-A.set_punchim_lower(1203,1250)
-A.set_punchim_upper(2459,1314)
-A.LunusPunchim()
-A.set_windim_lower(100,100)
-A.set_windim_upper(2362,2426)
-A.LunusWindim()
-A.set_thresholds(0,50)
-A.LunusThrshim()
-A.set_modeim_width(20)
-A.LunusModeim()
+A.set_image(data)
+A.LunusPunchim(1203,1250,2459,1314)
+A.LunusWindim(100,100,2362,2426)
+A.LunusThrshim(0,50)
+A.LunusModeim(20)
 print "Mode filter finished."
-data2 = A.get_lunus_data();
+data2 = A.get_image();
 #dxtbx.format.FormatCBFMini.FormatCBFMini.as_file(detector,beam,gonio,scan,data,path,header_convention="GENERIC_MINI",det_type="GENERIC")
 FormatCBFMini.as_file(detector,beam,gonio,scan,data2,"g150aich_274_6_1_lunus_00001.cbf")
