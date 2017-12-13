28a525101d16bf8c72c5901245f077067494f119
Mike Wall <mewall00@gmail.com>
2017-12-13 15:26:37 -0700

    Modify makemake to provide separate build recipes for mpi and non-mpi methods.

--- test/unit_tests/TestLunus.sh
+++ test/unit_tests/TestLunus.sh
@@ -1,2 +1,6 @@
-lunus test_inputs.sh 
+if [ "${LUNUS_MPI}" == "YES" ];
+  mpirun -n 1 lunus test_inputs.sh
+else
+  lunus test_inputs.sh 
+fi
 diff ../proc/snc_newhead_lunus_00001.img ../ref/snc_newhead_lunus_00001.img
