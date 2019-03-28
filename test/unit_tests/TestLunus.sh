8aae12a8c5185358cdc3a0ef26ab4d4f49a1ad1f
Mike Wall <mewall@lanl.gov>
2019-03-28 09:56:12 -0600

    bc not available in gitlab server. try awk instead.

--- test/unit_tests/TestLunus.sh
+++ test/unit_tests/TestLunus.sh
@@ -5,7 +5,7 @@ anisolt lattices/TestLunus.lat lattices/TestLunus_aniso.lat
 c=`corrlt lattices/TestLunus_aniso.lat ../../ref/TestLunusRef_aniso.lat`
 #diff lattices/TestLunus.lat ../../ref/TestLunusRef.lat
 cd -
-if (( $(echo "$c >= 0.999999" | bc -l) )); then
+if (( $(awk -v n1="$c" -v n2="0.99999" 'BEGIN {print (n1<n2?"0":"1")}') )); then
   rslt=0
 else
   echo "Failed correlation test, c = $c"
