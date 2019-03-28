39a172910110612f99273b3bed8c04c5ce4db078
Mike Wall <mewall@lanl.gov>
2019-03-28 09:47:19 -0600

    Allow for a small difference in the correlation in passing the TestLunus.sh test

--- test/unit_tests/TestLunus.sh
+++ test/unit_tests/TestLunus.sh
@@ -5,7 +5,7 @@ anisolt lattices/TestLunus.lat lattices/TestLunus_aniso.lat
 c=`corrlt lattices/TestLunus_aniso.lat ../../ref/TestLunusRef_aniso.lat`
 #diff lattices/TestLunus.lat ../../ref/TestLunusRef.lat
 cd -
-if [ $c == 1 ]; then
+if (( $(echo "$c >= 0.999999" | bc -l) )); then
   rslt=0
 else
   echo "Failed correlation test, c = $c"
