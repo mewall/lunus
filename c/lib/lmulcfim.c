3b3585ef0582f661a62b5afd2d46b9e838552aae
Mike Wall <mewall@lanl.gov>
2019-06-12 13:40:19 -0600

    add multipanel support for remaining methods so far implemented in lunus.process

--- c/lib/lmulcfim.c
+++ c/lib/lmulcfim.c
@@ -9,52 +9,63 @@
 
 #include<mwmask.h>
 
-int lmulcfim(DIFFIMAGE *imdiff)
+int lmulcfim(DIFFIMAGE *imdiff_in)
 {
-	size_t
-	  i,j,n,m,ct,r,c,index;
+  size_t
+    i,j,n,m,ct,r,c,index;
 
-	int
-	  return_value = 0;
+  int
+    return_value = 0;
 
-        float sum;
+  float sum;
 
-	for (j = 0;j<imdiff->vpixels;j++) {
-	  for (i = 0;i<imdiff->hpixels;i++) {
-	    index = j*imdiff->hpixels+i;
-	    if ((imdiff->image[index] != imdiff->overload_tag) &&
-		(imdiff->image[index] != imdiff->ignore_tag)) {
-	      //	      imdiff->image[index] = (imdiff->image[index]-imdiff->value_offset)*imdiff->correction[index]+imdiff->value_offset;
-	      if (imdiff->image[index] != 0) {
-		imdiff->image[index] = (imdiff->image[index]-imdiff->value_offset)*imdiff->correction[index]+imdiff->value_offset;
-	      } else {
-		size_t ct = 0;
-		sum=0.0;
-		for (n=-1;n<=1;n++) {
-		  r = j + n;
-		  for (m=-1;m<=1;m++) {
-		    c = i + m;
-		    if (!((r < 0) || (r > imdiff->vpixels) || (c < 0) ||
- 			  (c > imdiff->hpixels))) {
-		      size_t imd_index;
-		      imd_index = index + n*imdiff->hpixels + m;
-		      if ((imdiff->image[imd_index] != imdiff->overload_tag) &&
-			  (imdiff->image[imd_index] != imdiff->ignore_tag)) {
-			sum += (float)(imdiff->image[imd_index]-imdiff->value_offset);
-			ct++;
-		      }
-		    }
+  int pidx;
+
+  DIFFIMAGE *imdiff;
+
+  for (pidx = 0; pidx < imdiff_in->num_panels; pidx++) {
+    imdiff = &imdiff_in[pidx];
+    index = 0;
+
+    if (imdiff->slist == NULL) lslistim(imdiff);
+
+    struct xyzcoords s;
+    for (j = 0;j<imdiff->vpixels;j++) {
+      for (i = 0;i<imdiff->hpixels;i++) {
+	index = j*imdiff->hpixels+i;
+	if ((imdiff->image[index] != imdiff->overload_tag) &&
+	    (imdiff->image[index] != imdiff->ignore_tag)) {
+	  //	      imdiff->image[index] = (imdiff->image[index]-imdiff->value_offset)*imdiff->correction[index]+imdiff->value_offset;
+	  if (imdiff->image[index] != 0) {
+	    imdiff->image[index] = (imdiff->image[index]-imdiff->value_offset)*imdiff->correction[index]+imdiff->value_offset;
+	  } else {
+	    size_t ct = 0;
+	    sum=0.0;
+	    for (n=-1;n<=1;n++) {
+	      r = j + n;
+	      for (m=-1;m<=1;m++) {
+		c = i + m;
+		if (!((r < 0) || (r > imdiff->vpixels) || (c < 0) ||
+		      (c > imdiff->hpixels))) {
+		  size_t imd_index;
+		  imd_index = index + n*imdiff->hpixels + m;
+		  if ((imdiff->image[imd_index] != imdiff->overload_tag) &&
+		      (imdiff->image[imd_index] != imdiff->ignore_tag)) {
+		    sum += (float)(imdiff->image[imd_index]-imdiff->value_offset);
+		    ct++;
 		  }
 		}
-		if (ct>0) {
-		  imdiff->image[index] = (IMAGE_DATA_TYPE)(sum*imdiff->correction[index]/(float)ct) + imdiff->value_offset;
-		}
 	      }
-	      if (imdiff->image[index] == 0) {imdiff->image[index] = imdiff->ignore_tag;}
+	    }
+	    if (ct>0) {
+	      imdiff->image[index] = (IMAGE_DATA_TYPE)(sum*imdiff->correction[index]/(float)ct) + imdiff->value_offset;
 	    }
 	  }
+	  if (imdiff->image[index] == 0) {imdiff->image[index] = imdiff->ignore_tag;}
 	}
-
-	return(return_value);
+      }
+    }
+  }
+  return(return_value);
 
 }
