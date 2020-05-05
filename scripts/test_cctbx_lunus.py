d585b0d76b2e8bd2343523b53244c6798c850b09
Mike Wall <mewall@lanl.gov>
2020-05-05 18:30:41 -0400

    add test for lunus with CCTBX i/o

--- scripts/test_cctbx_lunus.py
+++ scripts/test_cctbx_lunus.py
@@ -27,6 +27,14 @@ except ValueError:
 else:
     out_name = args.pop(idx).split("=")[1]
 
+# Reference image file
+try:
+    idx = [(a.find("reference")==0) for a in args].index(True)
+except ValueError:
+    ref_name = None
+else:
+    ref_name = args.pop(idx).split("=")[1]
+
 img = dxtbx.load(inp_name)
 
 detector = img.get_detector()
@@ -37,7 +45,7 @@ scan = img.get_scan()
 A = LunusDIFFIMAGE()
 data = img.get_raw_data()
 data_np = data.as_numpy_array()
-hist=np.histogram(data,range=(1,50),bins=50,density=False)
+hist=np.histogram(data,range=(0,50),bins=50,density=False)
 print(hist[0:50])
 print(data[0])
 A.set_image(data)
@@ -64,5 +72,15 @@ A.LunusThrshim()
 A.LunusModeim()
 print("Mode filter finished.")
 data2 = A.get_image();
+if not ref_name is None:
+    ref = dxtbx.load(ref_name)
+    ref_data_np = ref.get_raw_data().as_numpy_array().flatten()
+    data2_np = data2.as_numpy_array().flatten()
+#    data2_np_rand = np.random.random_sample((len(data2_np),))
+    cc = np.corrcoef(ref_data_np,data2_np)[0,1]
 #dxtbx.format.FormatCBFMini.FormatCBFMini.as_file(detector,beam,gonio,scan,data,path,header_convention="GENERIC_MINI",det_type="GENERIC")
 FormatCBFMini.as_file(detector,beam,gonio,scan,data2,out_name)
+if np.abs(cc - 1.0) < 0.00001:
+    sys.exit(0)
+else: 
+    sys.exit(1)
