a19c331ccc4370bcb2bf24568969e102bf526cc8
Mike Wall <mewall@lanl.gov>
2020-05-05 18:35:14 -0400

    add python module to loaded modules in the test script

--- test/unit_tests/noTestCCTBXLunus.sh
+++ test/unit_tests/noTestCCTBXLunus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -c
 module load gcc/9.1.0
 module load cuda/9.2.148
+module load python/3.7.0
 source ../../../../build/setpaths.sh
 libtbx.python ../../scripts/test_cctbx_lunus.py input=../data/TestImageCBF_00001.cbf output=TestCCTBXLunus_00001.cbf reference=../ref/TestCCTBXLunusRef_00001.cbf
 if [ $? == 0 ]; then
