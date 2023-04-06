c185c43da9679f88eda720fe56033579430d2829
Mike Wall <mewall@lanl.gov>
2023-04-06 13:22:50 -0600

    Notebook changes

--- examples/tutorials/crystalline_MD_analysis_diffuse/code/preprocess.sh
+++ examples/tutorials/crystalline_MD_analysis_diffuse/code/preprocess.sh
@@ -12,14 +12,16 @@ fi
 #############################
 
 # truncate data if resolution range is provided  
-if [ ! -z "$3" ] && [ ! -z $4 ]; then
+if [ ! -z "$3" ] && [ ! -z "$4" ]; then
   lunus.cullreslt $f ${f%.lat}_culled.lat $3 $4
 fi
 # symmetrize
 lunus.symlt ${f%.lat}_culled.lat ${f%.lat}_culled_sym.lat $2
 # calculated experimental data anisotropic component
-lunus.anisolt ${f%.lat}_culled_sym.lat ${f%.lat}_culled_sym_aniso.lat $(cat cell.txt)
-# calculate isotropic profile for experimental data
-lunus.avgrlt ${f%.lat}_culled.lat ${f%.lat}_culled_iso.rf $(cat cell.txt)
-lunus.binasc 2 < ${f%.lat}_culled_iso.rf > ${f%.lat}_culled_iso.dat
+if [ ! -z "$5" ]; then
+  lunus.anisolt ${f%.lat}_culled_sym.lat ${f%.lat}_culled_sym_aniso.lat
+else
+  lunus.anisolt ${f%.lat}_culled_sym.lat ${f%.lat}_culled_sym_aniso.lat $5
+fi
+
 
